5810715349127b07736cfb9f1fb01574c8363e55
Jichao Yang <jasonyangjichao@163.com>
2018-07-22 17:18:53 +0800

    first workable version 1.0

--- generate_input.m
+++ generate_input.m
@@ -1,4 +1,19 @@
-function inp = generate_input(problem_type, pressure_unit, pressure_val, temp_unit, temp_val, reactant_amount_unit, reactant_temp_unit, fuel_name, fuel_amount, fuel_temp, oxid_name, oxid_amount, oxid_temp, output)
+function inp = generate_input(list)
+
+problem_type = list(1);             problem_type = string(problem_type{1});
+pressure_unit = list(2);            pressure_unit = string(pressure_unit{1});
+pressure_val = list(3);             pressure_val = string(pressure_val{1});
+temp_unit = list(4);                temp_unit = string(temp_unit{1});
+temp_val = list(5);                 temp_val = string(temp_val{1});
+reactant_amount_unit = list(6);     reactant_amount_unit = string(reactant_amount_unit{1});
+reactant_temp_unit = list(7);       reactant_temp_unit = string(reactant_temp_unit{1});
+fuel_name = list(8);                fuel_name = string(fuel_name{1});
+fuel_amount = list(9);              fuel_amount = string(fuel_amount{1});
+fuel_temp = list(10);               fuel_temp = string(fuel_temp{1});
+oxid_name = list(11);               oxid_name = string(oxid_name{1});
+oxid_amount = list(12);             oxid_amount = string(oxid_amount{1});
+oxid_temp = list(13);               oxid_temp = string(oxid_temp{1});
+output = list(14);                  output = string(output{1});
 
 problem_head = strcat('problem   ','\n');
 problem_body = strcat({'    '},problem_type,{'   '},'p,',pressure_unit,'=',pressure_val,{',  '},'t,',temp_unit,'=',temp_val,'\n');
