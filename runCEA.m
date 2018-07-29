467d75ba073d0a5b16b2a621f1072932434579e3
Jichao Yang <jasonyangjichao@163.com>
2018-07-29 11:05:12 +0300

    Update runCEA.m

--- runCEA.m
+++ runCEA.m
@@ -1,19 +1,62 @@
 clear; clc;
 
+% ================xls to inp================
+
+% read data from user created xls
 [~, ~, data] = xlsread('CEAdata.xls', 'sheet1');
 data = data(2:end, 2:end);
 [case_num, ~] = size(data);
 
+% convert data to .inp files
 for i = 1:case_num
-    case_data = data(i, :);
-    case_inp = generate_input(case_data);
-     fid = fopen(sprintf('%s%d.%s', 'case_', i, 'inp'), 'wt');
-     fprintf(fid, '%s', case_inp);
+    case_inp = generate_input(data(i, :));
+    fid = fopen(sprintf('%s%d.%s', 'case_', i, 'inp'), 'wt');
+    fprintf(fid, '%s', case_inp);
     fclose(fid);
 end
 
-% =========inp to plt===========
+% ================inp to plt================
+
+% prompt message between manual selection and autorun
 promptMessage = sprintf('Do you wish to run all .inp files?');
+titleBarCaption = 'Select Action';
+button = questdlg(promptMessage, titleBarCaption, 'Manually Select', 'Run All Files', 'Run All Files');
+if strcmpi(button, 'Manually Select')
+    autoruninp = false;
+else
+    autoruninp = true;
+end
+
+% generate list of .inp file names to be processed
+if autoruninp == false
+    % select multiple files through dialogue box
+    [FileNames,~] = uigetfile('*.inp', 'Chose files to load:', 'MultiSelect', 'on');
+    if ischar(FileNames)
+        FileNames = {FileNames};
+    end
+    inp_num = length(FileNames);
+    for i = 1:inp_num
+        name = FileNames{i};
+        name = name(1:end-4);
+        FileNames{i} = name;
+    end
+else
+    inp_num = case_num;
+    FileNames = cell([1,inp_num]);
+    for i = 1:inp_num
+        FileNames{i} = sprintf('%s%d', 'case_', i);
+    end
+end
+
+% run CEA to process .inp files selected
+for i = 1:inp_num
+    dos(sprintf('%s%s%s', 'echo ', FileNames{i}, ' | FCEA2 > nul'));
+end
+
+% ================plt to xls================
+
+% prompt message between manual selection and autorun
+promptMessage = sprintf('Do you wish to run all .plt files?');
 titleBarCaption = 'Select Action';
 button = questdlg(promptMessage, titleBarCaption, 'Manually Select', 'Run All Files', 'Run All Files');
 if strcmpi(button, 'Manually Select')
@@ -22,124 +65,47 @@ else
     autorunplt = true;
 end
 
+% read all .plt files selected by user
 if autorunplt == false
-    [FileNames, PathNames] = uigetfile('*.inp', 'Chose files to load:', 'MultiSelect', 'on'); % select multiple files
+    [FileNames,~] = uigetfile('*.plt', 'Chose files to load:', 'MultiSelect', 'on');
     if ischar(FileNames)
-        N = 1;
-    else
-        N = length(FileNames);
+        FileNames = {FileNames};
     end
-    for file = 1:N % for loop to cycle through all files
-        if N == 1
-            fullpath = [PathNames, FileNames];
-        else
-            fullpath = [PathNames, FileNames{file}]; % define the path to file
-        end
-    end
- 
-    for i = 1:length(FileNames)
-        name = FileNames{i};
-        name = name(1:end - 4);
-         dos(sprintf('%s%s%s', 'echo ', name, ' | FCEA2 > nul'));
+    plt_num = length(FileNames);
+    plt_val = cell([i,1]);
+    for i = 1:plt_num
+        full_path = ['./', FileNames{i}];
+        % read .plt file as string
+        line_read = textscan(fopen(full_path, 'r'), '%s%s%s%[^\n\r]', 'delimiter', '\t');
+        plt_val{i} = line_read{1};
     end
 else
-    for i = 1:case_num
-         dos(sprintf('%s%d%s', 'echo case_', i, ' | FCEA2 > nul'));
+    plt_num = case_num;
+    plt_val = cell([i,1]);
+    for i = 1:plt_num
+        full_path = sprintf('%s%s%d%s', '.\', 'case_', i, '.plt');
+        % read .plt file as string
+        line_read = textscan(fopen(full_path, 'r'), '%s%s%s%[^\n\r]', 'delimiter', '\t'); % read the file as strings
+        plt_val{i} = line_read{1};
     end
 end
 
-% =========plt to xls==========
-promptMessage = sprintf('Do you wish to run all .plt files?');
-titleBarCaption = 'Select Action';
-numberOfUsers = 1;
-buttonSelections = zeros(1, numberOfUsers); % Preallocate.
-for userNumber = 1 : numberOfUsers
-    button = questdlg(promptMessage, titleBarCaption, 'Manually Select', 'Run All Files', 'Run All Files');
-    if strcmpi(button, 'Manually Select')
-        autorunplt = false;
-    else
-        autorunplt = true;
-    end
+% write .plt data to xls
+header = strsplit(plt_val{1}{1});
+xlswrite('CEAdata.xls', header, 'Sheet2', 'A1');
+
+for i = 1:plt_num
+    plt_val{i} = strsplit(plt_val{i}{2});
+    xlswrite('CEAdata.xls', plt_val{i}, 'Sheet2', sprintf('%s%d', 'B', i+1));
+    xlswrite('CEAdata.xls', sprintf('%d', i), 'Sheet2', sprintf('%s%d', 'A', i+1));
 end
 
-if autorunplt == false
-    [FileNames, PathNames] = uigetfile('*.plt', 'Chose files to load:', 'MultiSelect', 'on'); % select multiple files
-    if ischar(FileNames)
-        N = 1;
-    else
-        N = length(FileNames);
-    end
-    for file = 1:N % for loop to cycle through all files
-        if N == 1
-            fullpath = [PathNames, FileNames];
-        else
-            fullpath = [PathNames, FileNames{file}]; % define the path to file
-        end
-        formatSpec = '%s%s%s%[^\n\r]'; % for textscan
-        delimiter = '\t';
-        startRow = 3; % start of data
-        endRow = 4; % end of data
-        fileID = fopen(fullpath, 'r'); % open the file
-        line_read = textscan(fileID, formatSpec, 'delimiter', delimiter); % read the file as strings
-        % T = cellstr(T);
-        result = strsplit(line_read{1}{2});
-        header = strsplit(line_read{1}{1});
-        % # output file name
-        fName = fullfile(pwd, 'CEAdata.xls');
-
-        % # create Excel COM Server
-        Excel = actxserver('Excel.Application');
-        Excel.Visible = false;
-        data = ones(10, 4); % Sample 2-dimensional data
-
-        xlswrite('CEAdata.xls', result, 'Sheet2', sprintf('%s%d', 'B', (file + 1))); % Write column header
-        xlswrite('CEAdata.xls', file, 'Sheet2', sprintf('%s%d', 'A', (file + 1))); % Write column header
-    end
-    xlswrite('CEAdata.xls', header, 'Sheet2', 'A1'); % Write column header
-    % # close Excel
-    Excel.Quit();
-else
-    for file = 1:case_num
-         FileNames{file} = sprintf('%s%d%s', 'case_', file, '.plt');
-    end
-    PathNames = '.\';
-    for file = 1:case_num
-        if case_num == 1
-            fullpath = [PathNames, FileNames];
-        else
-            fullpath = [PathNames, FileNames{file}]; % define the path to file
-        end
-        formatSpec = '%s%s%s%[^\n\r]'; % for textscan
-        delimiter = '\t';
-        startRow = 3; % start of data
-        endRow = 4; % end of data
-        fileID = fopen(fullpath, 'r'); % open the file
-        line_read = textscan(fileID, formatSpec, 'delimiter', delimiter); % read the file as strings
-        % T = cellstr(T);
-        result = strsplit(line_read{1}{2});
-        header = strsplit(line_read{1}{1});
-        % # output file name
-        fName = fullfile(pwd, 'CEAdata.xls');
-
-        % # create Excel COM Server
-        Excel = actxserver('Excel.Application');
-        Excel.Visible = false;
-        data = ones(10, 4); % Sample 2-dimensional data
-
-        xlswrite('CEAdata.xls', result, 'Sheet2', sprintf('%s%d', 'B', (file + 1))); % Write column header
-        xlswrite('CEAdata.xls', file, 'Sheet2', sprintf('%s%d', 'A', (file + 1))); % Write column header
-        xlswrite('CEAdata.xls', header, 'Sheet2', 'A1'); % Write column header
-        % # close Excel
-        Excel.Quit();
-    end
-end
-
-% ==============calculate necessary variables=============
+% calculate necessary variables
 xlswrite('CEAdata.xls', {'Big_gamma','C_star_theoretical','C_F_0','ISP_theoretical'}, 'Sheet2', 'I1');
 for i = 1:case_num
     [gamb,cth,cf,ispth] = myequationo(i);
     xlswrite('CEAdata.xls', [gamb,cth,cf,ispth], 'Sheet2', sprintf('%s%d', 'I', i+1));
 end
 
+% open xls when finished
 winopen('CEAdata.xls');
-
