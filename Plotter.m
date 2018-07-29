8c894ad63c0ce984e10f68b236927ef9ba7e137c
Jichao Yang <jasonyangjichao@163.com>
2018-07-29 12:41:01 +0300

    Update Plotter.m

--- Plotter.m
+++ Plotter.m
@@ -3,17 +3,26 @@ clear; clc;
                 % 'Sample', [1 50]);
              density1 = 2700; %str2num(answer{1});
              
-    answer1 = inputdlg('Enter Fuel Composition Percentage:',...
+    answer1 = inputdlg('Enter Fuel Composition Percentage for Test 1:',...
                  'Sample', [1 50]);
              percent = str2num(answer1{1});
-             
+            
+    answer1 = inputdlg('Enter Fuel Composition Percentage for Test 2:',...
+                 'Sample', [1 50]);
+             percent1 = str2num(answer1{1});         
+     
+    answer1 = inputdlg('Enter Fuel Composition Percentage for Test 3:',...
+                 'Sample', [1 50]);
+             percent2 = str2num(answer1{1});         
     %answer2 = inputdlg('Enter Fuel Density 2:',...
                  %'Sample', [1 50]);
              density2 = 834; %str2num(answer2{1});
             
 density = 1/(((percent)/100)/(density1)+((100 - percent)/100)/(density2));
-             
-dataset = xlsread('CEAdata.xls','Sheet2','B1:L99');
+density1 = 1/(((percent1)/100)/(density1)+((100 - percent1)/100)/(density2));
+density2 = 1/(((percent2)/100)/(density1)+((100 - percent2)/100)/(density2));
+
+dataset = xlsread('CEAdata.xls','Sheet2','B1:N99');
 
 x = dataset(:,7); % Untested
 y = dataset(:,2); % Untested
@@ -35,10 +44,20 @@ xlabel(ax2,'\phi')
 
 
 y3 = density * dataset(:,11); % Untested
+y4 = density * dataset(:,12); % Untested
+y5 = density * dataset(:,13); % Untested
 
 ax3 = subplot(3,1,3); % bottom subplot
 p = plot(ax3,x,y3);
 title(ax3,'\rho Isp vs \phi')
 ylabel(ax3,'\rho Isp')
 xlabel(ax3,'\phi')
-%p(1).Marker = '*';
+
+hold on
+plot(ax3,x,y4)
+
+hold on
+plot(ax3,x,y5)
+
+hold off
+p(1).LineWidth = 2;
