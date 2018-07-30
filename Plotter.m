55e04f04cfab90e56a9e7caf291396feb9ead1f2
Jichao Yang <jasonyangjichao@163.com>
2018-07-30 11:40:57 +0300

    Update Plotter.m

--- Plotter.m
+++ Plotter.m
@@ -30,7 +30,7 @@ density2 = 1/(((percent2)/100)/(densityAL)+((100 - percent2)/100)/(densityWAX));
 
 rhof = 1/((((rho)/100)/density)+((1-(rho/100))/1.141));
 rhof1 = 1/((((rho1)/100)/density1)+((1-(rho1/100))/1.141));
-rhof2 = 1/((((rho2)/100)/density1)+((1-(rho2/100))/1.141));
+rhof2 = 1/((((rho2)/100)/density2)+((1-(rho2/100))/1.141));
 
 %====================FirstGraph====================%
 x = datasetphi(:,7); % Untested
@@ -52,6 +52,7 @@ plot(ax1,x,t2)
 legend(sprintf('%d%s',percent','% AL'),sprintf('%d%s',percent1,'% AL'),sprintf('%d%s',percent2,'% AL'))
 hold off
 p(1).LineWidth = 2;
+
 %====================SecondGraph====================%
 
 isp = dataset(:,7); % First trial temperature
@@ -59,7 +60,7 @@ isp1 = dataset(:,8); % Second trial temperature
 isp2 = dataset(:,9); % Third trial temperature
 
 ax2 = subplot(3,1,2); % Top subplot
-p = plot(ax2,x,isp);
+l = plot(ax2,x,isp);
 title(ax2,'Isp vs \phi')
 ylabel(ax2,'Isp')
 xlabel(ax2,'\phi')
@@ -71,29 +72,30 @@ hold on
 plot(ax2,x,isp2)
 legend(sprintf('%d%s',percent','% AL'),sprintf('%d%s',percent1,'% AL'),sprintf('%d%s',percent2,'% AL'))
 hold off
-p(1).LineWidth = 2;
+l(1).LineWidth = 2;
 
 %====================ThirdGraph====================%
 
-y3 = (1/100) .* rho .* dataset(:,7); % Untested
-y4 = (1/100) .* rho1 .* dataset(:,8); % Untested
-y5 = (1/100) .* rho2 .* dataset(:,9); % Untested
+y3 = rhof(10) .* dataset(:,7); % Untested
+y4 = rhof1(10) .* dataset(:,8); % Untested
+y5 = rhof2(10) .* dataset(:,9); % Untested
 
 ax3 = subplot(3,1,3); % bottom subplot
-p = plot(ax3,x,y3);
+m = plot(ax3,x,y3);
 title(ax3,'\rho Isp vs \phi')
 ylabel(ax3,'\rho Isp')
 xlabel(ax3,'\phi')
 
 hold on
-plot(ax3,x,y4)
+plot(ax3,x,y4);
 
 hold on
-plot(ax3,x,y5)
+plot(ax3,x,y5);
 legend(sprintf('%d%s',percent','% AL'),sprintf('%d%s',percent1,'% AL'),sprintf('%d%s',percent2,'% AL'))
 
+m(1).LineWidth = 2;
 hold off
 
-p(1).LineWidth = 2;
+
 
 
